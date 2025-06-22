ab7098300ee08414736de0972b9566b62b013493
Nikita Naskalnyi <niknnm06@gmail.com>
2025-06-22 02:38:22 +0200

    add all

---
+++ src/middlewares/isValidId.js
@@ -0,0 +1,14 @@
+import { isValidObjectId } from 'mongoose';
+import createError from 'http-errors';
+
+const isValidId = (req, res, next) => {
+  const { contactId } = req.params;
+
+  if (!isValidObjectId(contactId)) {
+    return next(createError(400, `${contactId} is not a valid MongoDB ID`));
+  }
+
+  next();
+};
+
+export default isValidId;
