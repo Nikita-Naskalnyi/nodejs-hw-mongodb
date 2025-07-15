523b49398d16725fe4095e5d44fe527c395f04d6
Nikita Naskalnyi <niknnm06@gmail.com>
2025-07-15 17:20:43 +0200

    change path

--- generateToken.js
+++ generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const token = jwt.sign(
   { email: 'test3@example.com' },
-  'VOQjLdrpG1TWCHhDzv3o', // Твой JWT_SECRET
+  'VOQjLdrpG1TWCHhDzv3o',
   { expiresIn: '5m' }
 );
 
